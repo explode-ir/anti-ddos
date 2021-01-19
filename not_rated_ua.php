0b0ab24cb3814e0b9d4da33a9ff5ee64ed93feb2
explode TM <77636109+explode-ir@users.noreply.github.com>
2021-01-19 11:12:37 +0330

    Add files via upload

---
+++ not_rated_ua.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+    •explode TM 
+    •t.me/explode_ir
+    •t.me/explode_tm
+ */
+
+$not_rated_ua = array(
+        'explode Uptime bot.+',
+        'Googlebot', 
+        'Bingbot',
+        'Baiduspider',
+        'YandexBot',
+        'facebot',
+        'facebookexternalhit/1.1 (+http://www.facebook.com/externalhit_uatext.php)',
+        'ia_archiver'
+);
+
+?>
